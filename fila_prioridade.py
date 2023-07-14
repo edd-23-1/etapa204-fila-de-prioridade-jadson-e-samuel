e0a925461173dadebc0ad5a9dbbe72ed488ca761
Jadson Leitão <114675109+JadsonTSI@users.noreply.github.com>
2023-07-14 07:30:47 -0300

    Update fila_prioridade.py

--- fila_prioridade.py
+++ fila_prioridade.py
@@ -96,5 +96,5 @@ class FilaPrioridade:
     # retorna a quantidade de elementos na fila de prioridade
     # se a fila de prioridade estiver vazia, retorna ZERO
     def size(self) -> int:
-
-        return 0 if self.is_empty() else self.__qtdItens
+        return 0 if self.is_empty() 
+        else self.__qtdItens
