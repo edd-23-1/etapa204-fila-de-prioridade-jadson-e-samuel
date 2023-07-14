2ee687d95cd65998a7c86af56b56f5c5de354e37
Jadson Leitão <114675109+JadsonTSI@users.noreply.github.com>
2023-07-14 07:36:06 -0300

    Update fila_prioridade.py

--- fila_prioridade.py
+++ fila_prioridade.py
@@ -1,9 +1,5 @@
-class No:
-    def __init__(self, valor, prioridade):
-        self.valor = valor
-        self.prioridade = prioridade
-        self.prox = None
-
+# -*- coding:UTF-8 -*-
+from no import No
 
 class FilaPrioridade:
     """
@@ -19,25 +15,43 @@ class FilaPrioridade:
         self.__qtdItens = 0
         print(f"Criada EDD Fila de Prioridade com capacidade: {capacidade}")
 
+
+    # Retorna True se a fila de prioridade está vazia, False caso contrário
     def is_empty(self) -> bool:
-        return self.__qtdItens == 0
+        if self.__qtdItens == 0:
+            return True
+        else:
+            return False
 
+
+    
+    # retorna True se a fila de prioridade está cheia, False caso contrário
     def is_full(self) -> bool:
-        return self.__qtdItens == self.__capacidade
+        return True if self.__qtdItens == self.__capacidade else False
 
+
+    # Retorna uma referência para o primeiro item da fila de prioridade
+    # Caso a lista esteja vazia, retorna None
     def first(self) -> No:
-        return self.__inicio
+        return None if self.is_empty() else self.__inicio
 
+
+    # insere um item na fila de prioridade e retorna True, se o item for inserido
+    # se a fila de prioridade estiver cheia, lança uma exceção: raise Exception("mensagem de erro")
     def add(self, valor, prioridade) -> bool:
         no = No(valor, prioridade)
 
         if self.is_full():
             raise Exception("Lista cheia!")
 
-        if self.is_empty() or prioridade > self.__inicio.prioridade:
+        elif self.is_empty():
+            self.__inicio = no
+        
+        elif prioridade > self.__inicio.prioridade:
             no.prox = self.__inicio
             self.__inicio = no
-        else:
+
+        else:    
             pointer = self.__inicio
             while pointer.prox and prioridade <= pointer.prox.prioridade:
                 pointer = pointer.prox
@@ -45,9 +59,13 @@ class FilaPrioridade:
             no.prox = pointer.prox
             pointer.prox = no
 
-        self.__qtdItens += 1
+        self.__qtdItens += 1    
         return True
-
+    
+        
+    
+    # remove o primeiro item da fila de prioridade, caso não esteja vazia, e retorna o Nó
+    # se a fila de prioridade estiver vazia, lança uma exceção: raise Exception("mensagem de erro")
     def remove(self) -> No:
         if self.is_empty():
             raise Exception("Fila vazia")
@@ -57,17 +75,26 @@ class FilaPrioridade:
             self.__qtdItens -= 1
             return valor
 
+
+    # retorna uma lista de tuplas com os itens (valor e prioridade) da fila de prioridade 
+    # imprime os itens da fila de prioridade do primeiro para o último
+    # caso a fila de prioridade esteja vazia, imprime uma mensagem informando
+    # que a fila de prioridade está vazia e retorna uma lista vazia
     def display(self) -> list[tuple()]:
         if self.is_empty():
             print("Lista vazia!")
             return []
-
+        
         lista = []
         pointer = self.__inicio
         while pointer:
-            lista.append((pointer.valor, pointer.prioridade))
+            lista.append((pointer.dado, pointer.prioridade))
             pointer = pointer.prox
         return lista
+    
 
+    # retorna a quantidade de elementos na fila de prioridade
+    # se a fila de prioridade estiver vazia, retorna ZERO
     def size(self) -> int:
-        return self.__qtdItens
+
+        return 0 if self.is_empty() else self.__qtdItens
